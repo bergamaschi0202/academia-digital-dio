5a0d8c4ce617fb810e311a0d9722ecf7cf866cae
Arthur Bergamaschi Souza Sarria <bergamaschiarthur6@gmail.com>
2026-03-04 07:10:21 -0300

    Atualizando o perfil de acesso. Padrão: dev. E apagando o modelo do controller, service e repository do projeto para treinar

--- src/main/java/me/dio/academia/digital/controller/AvaliacaoFisicaController.java
+++ src/main/java/me/dio/academia/digital/controller/AvaliacaoFisicaController.java
@@ -12,17 +12,5 @@ import java.util.List;
 @RequestMapping("/avaliacoes")
 public class AvaliacaoFisicaController {
 
-  @Autowired
-  private AvaliacaoFisicaServiceImpl service;
-
-  @PostMapping
-  public AvaliacaoFisica create(@RequestBody AvaliacaoFisicaForm form) {
-    return service.create(form);
-  }
-
-  @GetMapping
-  public List<AvaliacaoFisica> getAll(){
-    return service.getAll();
-  }
 
 }
